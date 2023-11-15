480cd315c8a9f896569d5bd765ac537213c9697c
Saksham091 <saksham.agarwal@kalvium.community>
2023-11-15 17:20:26 +0530

    Profile Component Created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
 import Signup from './Components/Sign Up/Signup';
+import Profile from './Components/Profile/Profile';
 import Home from './Components/Home/Home';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
