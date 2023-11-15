85a760a67d7b3989f0fcab1bf346f03f7762a3ff
Saksham091 <saksham.agarwal@kalvium.community>
2023-11-15 17:18:55 +0530

    Home Component Created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
 import Signup from './Components/Sign Up/Signup';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
